6c5a82e91479390fa931bc27fe7f04d999ca5f8e
Mo0sel Maker <marlus.dx@gmail.com>
2026-07-13 20:50:50 -0500

    Update nd-companion.js

---
+++ scripts/nd-companion.js
@@ -0,0 +1,8 @@
+Hooks.once("init", () => {
+  console.log("%cN&D Companion", "color:#7dd3fc;font-size:16px;font-weight:bold;");
+  console.log("N&D Companion initialized.");
+});
+
+Hooks.once("ready", () => {
+  console.log("N&D Companion ready.");
+});
